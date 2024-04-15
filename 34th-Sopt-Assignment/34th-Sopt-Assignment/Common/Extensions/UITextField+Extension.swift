5963518d5a9a496547bbcd43081a6e58080e7db5
JinUng41 <ingjwjw@naver.com>
2024-04-15 23:43:50 +0900

    #1 - [Feat] extension 추가
    - UIFont
    - UITextField
    - UIView

---
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/Common/Extensions/UITextField+Extension.swift
@@ -0,0 +1,40 @@
+//
+//  UITextField+Extension.swift
+//  34th-Sopt-Assignment
+//
+//  Created by 김진웅 on 4/15/24.
+//
+
+import UIKit
+
+extension UITextField {
+    func addPadding(left: CGFloat? = nil, right: CGFloat? = nil) {
+        if let left {
+            leftView = UIView(frame: CGRect(x: 0, y: 0, width: left, height: 0))
+            leftViewMode = .always
+        }
+        if let right {
+            rightView = UIView(frame: CGRect(x: 0, y: 0, width: right, height: 0))
+            rightViewMode = .always
+        }
+    }
+    
+    func setTextField(forBackgroundColor: UIColor, forBorderColor: UIColor, forBorderWidth: CGFloat, forCornerRadius: CGFloat? = nil) {
+        clipsToBounds = true
+        layer.borderColor = forBorderColor.cgColor
+        layer.borderWidth = forBorderWidth
+        backgroundColor = forBackgroundColor
+        
+        if let cornerRadius = forCornerRadius {
+            layer.cornerRadius = cornerRadius
+        }  else {
+            layer.cornerRadius = 0
+        }
+    }
+    
+    func setPlaceholder(placeholder: String, fontColor: UIColor?, font: UIFont) {
+        attributedPlaceholder = NSAttributedString(string: placeholder,
+                                                        attributes: [.foregroundColor: fontColor!,
+                                                                     .font: font])
+    }
+}
