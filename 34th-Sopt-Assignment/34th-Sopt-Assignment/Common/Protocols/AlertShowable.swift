a0d7a2ec21befd5ea11b004bc5b88486a00b3847
JinUng41 <ingjwjw@naver.com>
2024-04-18 02:47:11 +0900

    #1 - [Feat] 정규식 검증 및 오류 경고창 띄우는 기능 추가

---
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/Common/Protocols/AlertShowable.swift
@@ -0,0 +1,21 @@
+//
+//  AlertShowable.swift
+//  34th-Sopt-Assignment
+//
+//  Created by 김진웅 on 4/18/24.
+//
+
+import UIKit
+
+protocol AlertShowable {
+    func showAlert(title: String, message: String)
+}
+
+extension AlertShowable where Self: UIViewController {
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "확인", style: .default)
+        alertController.addAction(confirmAction)
+        present(alertController, animated: true)
+    }
+}
