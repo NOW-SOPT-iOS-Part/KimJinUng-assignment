b3fb33ff3f250c8bcef7fd68582eaba39265838c
JinUng41 <ingjwjw@naver.com>
2024-05-10 13:57:26 +0900

    [Feat] 영화 목록을 보여줄 화면 초기 구현 (#5)
    - 목록을 위한 스크롤 뷰는 미구현

--- 34th-Sopt-Assignment/34th-Sopt-Assignment/Presentation/Home/Controllers/FindViewController.swift
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/Presentation/Home/Controllers/FindViewController.swift
@@ -7,23 +7,83 @@
 
 import UIKit
 
-final class FindViewController: UIViewController {
+import Then
+import SnapKit
 
+final class FindViewController: UIViewController {
+    
+    // MARK: - UIComponent
+    
+    private let backButton = UIButton()
+    
+    private let findTextField = TvingTextField(placeholder: "내용을 입력해주세요.")
+    
+    private let titleLabel = UILabel()
+    
+    // MARK: - Property
+    
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setUI()
+        setViewHierarchy()
+        setAutoLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Action
+    
+    @objc
+    private func backButtonTapped(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
     }
-    */
+}
 
+private extension FindViewController {
+    
+    // MARK: - SetUI
+    
+    func setUI() {
+        view.backgroundColor = .black
+        navigationItem.hidesBackButton = true
+        
+        backButton.do {
+            $0.setImage(UIImage(resource: .btnBefore), for: .normal)
+            $0.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        }
+        
+        titleLabel.do {
+            $0.setText("일일 박스오피스 순위", color: .white, font: .pretendard(weight: .six, size: 15))
+        }
+    }
+    
+    func setViewHierarchy() {
+        view.addSubviews(backButton, findTextField, titleLabel)
+    }
+    
+    // MARK: - AutoLayout
+    
+    func setAutoLayout() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        backButton.snp.makeConstraints {
+            $0.top.equalTo(safeArea)
+            $0.leading.equalTo(safeArea).offset(20)
+            $0.size.equalTo(20)
+        }
+        
+        findTextField.snp.makeConstraints {
+            $0.centerY.equalTo(backButton)
+            $0.leading.equalTo(backButton.snp.trailing).offset(10)
+            $0.trailing.equalTo(safeArea).offset(-20)
+            $0.height.equalTo(40)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(backButton.snp.bottom).offset(40)
+            $0.leading.equalTo(backButton)
+        }
+    }
 }
