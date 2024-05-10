4c3d446e2fe8b7bdaccfc22313b0f7e7683fec01
JinUng41 <ingjwjw@naver.com>
2024-05-10 13:56:05 +0900

    [Chore] 자잘한 디자인 코드 수정 (#5)

---
+++ 34th-Sopt-Assignment/34th-Sopt-Assignment/Presentation/Home/Controllers/FindViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FindViewController.swift
+//  34th-Sopt-Assignment
+//
+//  Created by 김진웅 on 5/10/24.
+//
+
+import UIKit
+
+final class FindViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
